e26b1783e4c66bd6159c2046f03f8897fefc3187
Francis Carlo Manlangit <francismanlangit13@gmail.com>
2023-06-22 01:33:05 +0800

    add logincode

---
+++ login/logincode.php
@@ -0,0 +1,45 @@
+<?php
+    include('../db_conn.php');
+
+    if(isset($_POST['login_btn'])){
+        $email = mysqli_real_escape_string($con, $_POST['email']);
+        $password = mysqli_real_escape_string($con, $_POST['password']);
+        $hashed_password = md5($password);
+        $login_query = "SELECT * FROM user WHERE email = '$email' AND password = '$hashed_password' AND user_status_id = 1 LIMIT 1";
+        $login_query_run = mysqli_query($con, $login_query);
+
+        if(mysqli_num_rows($login_query_run) > 0){
+            foreach($login_query_run as $data){
+                $user_id = $data['user_id'];
+                $full_name = $data['fname'].' '.$data['lname'];
+            }
+
+            $_SESSION['auth'] = true;
+            $_SESSION['auth_user'] = [
+                'user_id' =>$user_id,
+                'user_name' =>$full_name,
+            ];
+
+            if($_SESSION['auth'] == true){
+                $_SESSION['status'] = "Welcome $full_name!";
+                $_SESSION['status_code'] = "success";
+                header("Location: " . base_url . "admin");
+                exit(0);
+            }
+        }
+        else {
+            $_SESSION['status'] = "Invalid Email or Password";
+            $_SESSION['status_code'] = "error";
+            header("Location: " . base_url . "login");
+            exit(0);
+        }
+    }   
+    else {
+        $_SESSION['status'] = "You are not allowed to access this site";
+        $_SESSION['status_code'] = "error";
+        header("Location: " . base_url . "login");
+        
+        exit(0);
+    }
+
+?>
