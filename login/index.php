05090b6c1dc2e0d06de4a45edfb547149ff30d57
Francis Carlo Manlangit <francismanlangit13@gmail.com>
2023-06-24 01:03:35 +0800

    fix login final

--- login/index.php
+++ login/index.php
@@ -71,12 +71,12 @@
                                             <h5 class="card-title text-center pb-0 fs-4">Login to Your Account</h5>
                                             <p class="text-center small">Enter your email & password to login</p>
                                         </div>
-                                        <form class="row g-3 needs-validation" novalidate>
+                                        <form action="logincode.php" method="POST" class="row g-3 needs-validation" novalidate>
                                             <div class="col-12">
                                                 <label for="yourEmail" class="form-label">Email</label>
                                                 <div class="input-group has-validation">
                                                     <span class="input-group-text" id="inputGroupPrepend"><i class="bx bxs-envelope"></i></span>
-                                                    <input type="text" name="email" class="form-control" id="yourEmail" required>
+                                                    <input type="email" name="email" class="form-control" id="yourEmail" required>
                                                     <div class="invalid-feedback">Please enter your email.</div>
                                                 </div>
                                             </div>
@@ -95,7 +95,7 @@
                                                 </div>
                                             </div>
                                             <div class="col-12">
-                                                <button class="btn btn-primary w-100" type="submit">Login</button>
+                                                <button class="btn btn-primary w-100" name="login_btn" type="submit">Login</button>
                                             </div>
                                             <div class="col-12">
                                                 <p class="small mb-0">Forgot your account? <a href="forgot">Reset your password</a></p>
--- login/logincode.php
+++ login/logincode.php
@@ -5,7 +5,7 @@
         $email = mysqli_real_escape_string($con, $_POST['email']);
         $password = mysqli_real_escape_string($con, $_POST['password']);
         $hashed_password = md5($password);
-        $login_query = "SELECT * FROM user WHERE email = '$email' AND password = '$hashed_password' AND user_status_id = 1 LIMIT 1";
+        $login_query = "SELECT * FROM users WHERE email = '$email' AND password = '$hashed_password' LIMIT 1";
         $login_query_run = mysqli_query($con, $login_query);
 
         if(mysqli_num_rows($login_query_run) > 0){
