642d132b0aeb82c9c90e6811571009b68b7da051
Francis Carlo Manlangit <francismanlangit13@gmail.com>
2023-08-18 15:53:07 +0800

    fix login

--- login/logincode.php
+++ login/logincode.php
@@ -5,7 +5,7 @@
         $email = mysqli_real_escape_string($con, $_POST['email']);
         $password = mysqli_real_escape_string($con, $_POST['password']);
         $hashed_password = md5($password);
-        $login_query = "SELECT * FROM users WHERE email = '$email' AND password = '$hashed_password' LIMIT 1";
+        $login_query = "SELECT * FROM user WHERE email = '$email' AND password = '$hashed_password' LIMIT 1";
         $login_query_run = mysqli_query($con, $login_query);
 
         if(mysqli_num_rows($login_query_run) > 0){
@@ -14,6 +14,11 @@
                 $full_name = $data['fname'].' '.$data['lname'];
                 $user_type = $data['user_type_id'];
             }
+
+            $date = date('Y-m-d H:i:s');
+            $login_success = "Login";
+            $login_success_log = "success using email and password";
+            mysqli_query($con,"INSERT INTO user_log (user_id, type, log, logdate) values('".$user_id."','".$login_success."','".$login_success_log."','$date')");
 
             $_SESSION['auth'] = true;
             $_SESSION['auth_role'] = "$user_type";
@@ -30,6 +35,12 @@
             }
         }
         else {
+
+            $date = date('Y-m-d H:i:s');
+            $login_error = "Login";
+            $login_error_log = "failed invalid email or password";
+            mysqli_query($con,"INSERT INTO user_log (user_id, type, log, logdate) values('".$user_id."','".$login_success."','".$login_success_log."','$date')");
+
             $_SESSION['status'] = "Invalid Email or Password";
             $_SESSION['status_code'] = "error";
             header("Location: " . base_url . "login");
